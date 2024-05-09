0f1299724e017d4153697ee1e6852c40dabe1b27
Kaustubh W <kaustubh.wakhare@ibm.com>
2024-05-09 17:58:16 +0530

    Two way data binding lwc

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,4 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     fullName = "Zero To Hero";
+    title = 'aura';
+
+    changeTitle(event){
+        this.title = event.target.value;
+    }
 }
